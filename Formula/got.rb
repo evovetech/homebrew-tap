fa17b5c1fb027538e7ad7bdc7472ae7cb8880d16
Layne Penney <layne@evove.tech>
2018-01-02 03:40:10 -0600

    got version v0.2

---
+++ Formula/got.rb
@@ -0,0 +1,15 @@
+class Got < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/evovetech/got/releases/download/v0.2/got_0.2_darwin_amd64.tar.gz"
+  version "0.2"
+  sha256 "6aa8c7e67ed02b9ec9b34f46c8b654f4766516198c6d0a0d67c86867db461fd9"
+
+  def install
+    bin.install "got"
+  end
+
+  test do
+    
+  end
+end
