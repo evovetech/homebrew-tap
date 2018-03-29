83e91a623f92e20f8be4e48e06c1e8828db23620
Layne Penney <layne@evove.tech>
2018-03-29 14:43:18 -0500

    got v0.3

--- Formula/got.rb
+++ Formula/got.rb
@@ -1,15 +1,47 @@
+require 'rbconfig'
 class Got < Formula
   desc ""
-  homepage ""
-  url "https://github.com/evovetech/got/releases/download/v0.2/got_0.2_darwin_amd64.tar.gz"
-  version "0.2"
-  sha256 "6aa8c7e67ed02b9ec9b34f46c8b654f4766516198c6d0a0d67c86867db461fd9"
+  homepage "https://github.com/evovetech/got"
+  version "0.3"
+
+  if Hardware::CPU.is_64_bit?
+    case RbConfig::CONFIG['host_os']
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      :windows
+    when /darwin|mac os/
+      url "https://github.com/evovetech/got/releases/download/v0.3/got_0.3_darwin_amd64.zip"
+      sha256 "0e5f398a1f989ca190191639f69db612a65ace7c6f516768e55cf32118ffa67f"
+    when /linux/
+      url "https://github.com/evovetech/got/releases/download/v0.3/got_0.3_linux_amd64.tar.gz"
+      sha256 "46ffb4cdd547c316292e6d82278c49a3289f5f82bfda26eb45cd2260721ad2e7"
+    when /solaris|bsd/
+      :unix
+    else
+      :unknown
+    end
+  else
+    case RbConfig::CONFIG['host_os']
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      :windows
+    when /darwin|mac os/
+      url "https://github.com/evovetech/got/releases/download/v0.3/got_0.3_darwin_386.zip"
+      sha256 "d77940e55bb5bd6e85b74ac774bbab8d21afc55392cbfdfb6dacf7b44613ac34"
+    when /linux/
+      url "https://github.com/evovetech/got/releases/download/v0.3/got_0.3_linux_386.tar.gz"
+      sha256 "f00d5f7a246689fc8b258eefa697e68034769a1400d12f2a6e2361d8f9c78257"
+    when /solaris|bsd/
+      :unix
+    else
+      :unknown
+    end
+  end
 
   def install
     bin.install "got"
   end
 
   test do
-    
+    system "got"
   end
+
 end
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# homebrew-tap
+# Homebrew for evove.tech
+____
+
+Got -- git tools in golang
+====
+Usage:
+```bash
+# add repository to homebrew taps
+$ brew tap evovetech/tap
+
+# install from tap
+$ brew install got
+
+# use
+$ got --help
+```
